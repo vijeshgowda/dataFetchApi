51eca8a1817a0e7619b8cc84e9e71276f088bcca
vijeshgowda <vijesh.496@gmail.com>
2023-11-26 19:29:51 +0000

    minor app

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import Router from "./routes/routes.js";
 
-const app = express();
+var app = express();
 const PORT = 9000;
 
 app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(Router);
 
